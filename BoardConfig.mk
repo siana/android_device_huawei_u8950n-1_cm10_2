ba9da8607b253faf9dada7946687204b4a808ba9
josalaito <josalaito@hotmail.es>
2013-09-22 18:23:11 +0200

    FSTAB: Updated to unified system

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,7 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_ARCH_VARIANT_CPU := cortex-a5
 TARGET_ARCH_VARIANT_FPU := neon
 TARGET_CPU_SMP := true
